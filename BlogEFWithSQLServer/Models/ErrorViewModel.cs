734a2f377c9935411747b3653af7823db07d03b8
vivensNdatinya <vndatiny@harding.edu>
2018-11-19 06:45:10 -0600

    Initial commit

---
+++ BlogEFWithSQLServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogEFWithSQLServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
